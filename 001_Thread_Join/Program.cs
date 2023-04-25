594a820fa425e564c3fb938fca674b8e4225cfa1
Dina Nevhod <Dina_Nevhod@epam.com>
2023-04-25 11:29:47 +0200

    Add project files.

---
+++ 001_Thread_Join/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ThreadSample
+{
+    class Program
+    {
+        private static object locker = new object();
+        static void Function(string text)
+        {
+            lock (locker)
+            {
+                Console.WriteLine("ID " + text + " потік: {0}", Thread.CurrentThread.ManagedThreadId);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                for (int i = 0; i < 50; i++)
+                {
+                    Thread.Sleep(20);
+                    Console.Write(".");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(text + " потік завершився.");
+            }
+        }
+
+        static void Main() // Метод Main - виконується у первинному потоці.
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            
+            Thread thread1 = new Thread(()=>Function("Первинний"));
+            thread1.Start();
+            Thread thread2 = new Thread(() => Function("Вторинний"));
+            thread2.Start();
+            Thread thread3 = new Thread(() => Function("Третинний"));
+            thread3.Start();
+
+            // Очікування первинного потоку, завершення роботи вторинного потоку.
+            //thread.Join(); //TODO Зняти чи встановити коментар.
+
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task11.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task11._2
+{
+    internal class Program
+    {
+        static string outputText;
+        private static object locker = new object();
+        public static void FileReader(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                outputText = reader.ReadToEnd();
+            }
+        }
+        public static void FileWriter(string text)
+        {
+            lock (locker)
+            {
+                using (StreamWriter writer = new StreamWriter(@"File3.txt", true))
+                {
+                    writer.WriteLine(text);
+                }
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() => FileReader(@"File1.txt"));
+            thread1.Start();
+            Thread.Sleep(500);
+            thread1 = new Thread(() => FileWriter(outputText));
+            thread1.Start();
+            
+
+            Thread thread2 = new Thread(() => FileReader(@"File2.txt"));
+            thread2.Start();
+            Thread.Sleep(500);
+            thread2 = new Thread(() => FileWriter(outputText));
+            thread2.Start();
+            Console.WriteLine("Запис завершено");
+            Console.ReadLine();
+        }
+    }
+}
